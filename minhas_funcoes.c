f69342e80c787b58f6d3f809245b4f8d885db85b
joaoclemos <joaoclemos22@gmail.com>
2025-09-21 14:39:39 -0300

    Q2

---
+++ minhas_funcoes.c
@@ -0,0 +1,5 @@
+#include "minhas_funcoes.h"
+#include <stdio.h>
+void soma1(int *a){
+    (*a) ++;
+}
---
+++ principal.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "minhas_funcoes.h"
+
+int testaSoma1(int a){
+    soma1(&a);
+    return a;
+    }
