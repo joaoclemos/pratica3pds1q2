0e3bd5702767dbb578de596dc0ddf333dc88f143
joaoclemos <joaoclemos22@gmail.com>
2025-09-21 15:52:10 -0300

    Q6

--- minhas_funcoes.h
+++ minhas_funcoes.h
@@ -1,2 +1,3 @@
 void soma1(int *a);
 void troca(int *a, int *b);
+void DDD(long long tel);
--- principal.c
+++ principal.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include "minhas_funcoes.h"
 
-void testaTroca(float x, float y){
-    troca(&x, &y);
-    printf("%.4f %.4f", x, y);
-    }
+int main() {
+    int x;
+    printf("\nDigite um valor inteiro: ");
+    scanf("%d", &x);
+
+    soma1(&x);
+    printf("\nValor depois de chamar a função: %d", x);
+
+    float a, b;
+
+    printf("\nDigite os valores para troca: ");
+    scanf("%f %f", &a, &b);
+
+    troca(&a, &b);
+
+    printf("\nValores depois de chamar a função: %.4f %.4f", a, b);
+
+    long long tel;
+    printf("\nDigite o telefone: ");
+    scanf("%lld", &tel);
+    printf("\nDDD: %d", ddd(tel));
+
+    return 0;
+}
